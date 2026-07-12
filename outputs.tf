74722b558a937bc0b4ed6b26f702a13c2edac1c7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,49 @@
-output "managed_application_definitions" {
-  description = "All managed_application_definition resources"
-  value       = azurerm_managed_application_definition.managed_application_definitions
-}
 output "managed_application_definitions_authorization" {
-  description = "List of authorization values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.authorization]
+  description = "Map of authorization values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.authorization }
 }
 output "managed_application_definitions_create_ui_definition" {
-  description = "List of create_ui_definition values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.create_ui_definition]
+  description = "Map of create_ui_definition values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.create_ui_definition }
 }
 output "managed_application_definitions_description" {
-  description = "List of description values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.description]
+  description = "Map of description values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.description }
 }
 output "managed_application_definitions_display_name" {
-  description = "List of display_name values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.display_name]
+  description = "Map of display_name values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.display_name }
 }
 output "managed_application_definitions_location" {
-  description = "List of location values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.location]
+  description = "Map of location values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.location }
 }
 output "managed_application_definitions_lock_level" {
-  description = "List of lock_level values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.lock_level]
+  description = "Map of lock_level values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.lock_level }
 }
 output "managed_application_definitions_main_template" {
-  description = "List of main_template values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.main_template]
+  description = "Map of main_template values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.main_template }
 }
 output "managed_application_definitions_name" {
-  description = "List of name values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.name]
+  description = "Map of name values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.name }
 }
 output "managed_application_definitions_package_enabled" {
-  description = "List of package_enabled values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.package_enabled]
+  description = "Map of package_enabled values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.package_enabled }
 }
 output "managed_application_definitions_package_file_uri" {
-  description = "List of package_file_uri values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.package_file_uri]
+  description = "Map of package_file_uri values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.package_file_uri }
 }
 output "managed_application_definitions_resource_group_name" {
-  description = "List of resource_group_name values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.resource_group_name]
+  description = "Map of resource_group_name values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.resource_group_name }
 }
 output "managed_application_definitions_tags" {
-  description = "List of tags values across all managed_application_definitions"
-  value       = [for k, v in azurerm_managed_application_definition.managed_application_definitions : v.tags]
+  description = "Map of tags values across all managed_application_definitions, keyed the same as var.managed_application_definitions"
+  value       = { for k, v in azurerm_managed_application_definition.managed_application_definitions : k => v.tags }
 }
 
